d59d050849afe94ac01f91cd9dc77d8ab4e1f82b
Rustam Usmanov <rustamusmanov708@gmail.com>
2022-05-27 22:38:28 +0500

    Initial Commit

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TMDBApp extends StatelessWidget {
+  const TMDBApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tmdb/app.dart';
+
+void main() {
+  runApp(const TMDBApp());
+}
